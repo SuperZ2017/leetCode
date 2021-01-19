2928b53142a68221079d9daec47f8f198f25838a
zoucp <1351596798@qq.com>
2021-01-19 13:35:54 +0800

    commit

--- jianzhioffer/b03.java
+++ jianzhioffer/b03.java
@@ -4,8 +4,8 @@ package jianzhioffer;
  * 数组中重复的数字
  */
 public class b03 {
-
-    public int findRepeatNumber(int[] nums) {
+    // haha
+    public int findRepeatNumber(int[]  nums) {
 
 
         for (int i = 0; i < nums.length; i++) {
